5870f92a7f57c829c1f329d1102ea983e03b6d80
Oier Elola <oelola001@ikasle.ehu.eus>
2021-09-24 10:51:50 +0200

    Irudi eguneraketa

--- php/Credits.php
+++ php/Credits.php
@@ -12,7 +12,7 @@
 		<h1>Unax Lazkanotegui eta Oier Elola </h1>
 	 	<h1> Software Ingenieritza espezialitatea </h1>
 		<h1> Informatika Fakultatea </h1>
-		<br> <img src="../images/a.png" height="80" width="80" vspace="80" /> <img src="../images/a.png" height="80" width="80" />
+		<br> <img src="../images/a.png" height="" width="120" vspace="120" /> <img src="../images/ni.jpg" height="120" width="120" />
       
 
     </div>
