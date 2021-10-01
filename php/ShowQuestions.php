e9e206b07ffa128b2cc6c71bc182963d67e6ca92
Oier Elola <oelola001@ikasle.ehu.eus>
2021-10-01 18:51:17 +0200

    Aldaketak

--- php/ShowQuestions.php
+++ php/ShowQuestions.php
@@ -19,7 +19,7 @@
       echo '<table border=1> <tr> <th>GALDERAID<th>EPOSTA</th> <th>GALDERA</th> <th>ERANTZUN ZUZENA</th> <th>ERANTZUN OKERRA 1</th> <th>ERANTZUN OKERRA 2</th> <th>ERANTZUN OKERRA 3</th> <th>ZAILTASUNA</th> <th>ARLOA</th>';
       $giz->data_seek(0);
       while ($row=$giz->fetch_assoc()) {
-        echo '<tr><td>'.$row['GalderaID'].'</td> <td>'.$row->['Eposta'].'</td> <td>'.$row['Galdera'].'</td> <td>'.$row['erZuzena'].'</td> <td>'.$row['erOkerra1'].'</td> <td>'.$row['erOkerra2'].'</td> <td>'.$row['erOkerra3'].'</td> <td>'.$row['Zailtasuna'].'</td> <td>'.$row['Arloa'].'</td> </tr>';
+        echo '<tr><td>'.$row['GalderaID'].'</td> <td>'.$row['Eposta'].'</td> <td>'.$row['Galdera'].'</td> <td>'.$row['erZuzena'].'</td> <td>'.$row['erOkerra1'].'</td> <td>'.$row['erOkerra2'].'</td> <td>'.$row['erOkerra3'].'</td> <td>'.$row['Zailtasuna'].'</td> <td>'.$row['Arloa'].'</td> </tr>';
       }
 
       echo'</table>';
