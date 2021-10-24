56e982c550203e63ac807da70028b60857dfcf07
Oier Elola <oelola001@ikasle.ehu.eus>
2021-10-24 09:37:17 +0200

    Mezu aldaketak AddQuestion.php

--- php/AddQuestion.php
+++ php/AddQuestion.php
@@ -38,7 +38,7 @@ if (isset($_POST)){
   $trimgZail = trim($zailtasun);
   $trimgArloa = trim($arlo);
  
-  $patroia="/^[a-zA-Z]+[0-9]{3}@(ikasle\.ehu\.)(eus|es)$||^[a-zA-Z](\.[a-zA-Z]+@ehu\.)(eus|es)$/";
+  $patroia="^[a-zA-Z]+[0-9]{3}@(ikasle\.ehu\.)(eus|es)$||^[a-zA-Z](\.[a-zA-Z]+@ehu\.)(eus|es)$";
   //preg_match($patroia, $trimePosta, $matchesePosta);
   preg_match('/^.+$/', $trimePosta, $matchesePosta);
   //preg_match('^.{10}^', $trimgTestua, $matchesgTestua);
@@ -53,8 +53,8 @@ if (isset($_POST)){
   
 
   if ($matchesePosta && $matchesgTestua && $matcheseZuzena && $matcheseOkerra1 && $matcheseOkerra2 && $matcheseOkerra3 && $matchesgZail && $matchesgArloa){       
-   //if (preg_match($patroia,$trimePosta)==0){
-    if(filter_var($trimePosta,FILTER_VALIDATE_EMAIL)){ 
+   if (preg_match($patroia,$trimePosta)==0){
+    //if(filter_var($trimePosta,FILTER_VALIDATE_EMAIL)){ 
       //echo"<script> alert('ongi joan da')</script>";
       if (strlen($trimgTestua)>9){
       try{
